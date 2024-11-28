70a9554045761957450826144bd9c33058e56513
Otavio Ximarelli <ximarellio@gmail.com>
2024-11-27 22:46:33 -0300

    1

--- src/main/java/dev/otavio/UrlShorter/Main.java
+++ src/main/java/dev/otavio/UrlShorter/Main.java
@@ -4,18 +4,26 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final S3Client s3Client = S3Client.builder().build();
 
     @Override
-    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
-        String body = (String) input.get("body").toString();
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        if (input.get("body") == null) {
+            return buildErrorResponse(400, "Request body is missing");
+        }
+
+        String body = input.get("body").toString();
 
         Map<String, String> bodyMap;
         try {
@@ -26,22 +34,59 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
 
         String originalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get("expirationTime");
-        long expirationTimeInSeconds = Long.parseLong(expirationTime);
 
-        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+        if (originalUrl == null ||originalUrl.isBlank()) {
+            return buildErrorResponse(400, "'Original URL' is required");
+        }
+
+        if (expirationTime == null ||expirationTime.isBlank()) {
+            return buildErrorResponse(400, "'Expiration time' is required");
+        }
+
+        long expirationTimeInSeconds;
+        try {
+            expirationTimeInSeconds = Long.parseLong(expirationTime);
+        } catch (NumberFormatException e) {
+            return buildErrorResponse(400, "Field 'expirationTime' must be a valid number");
+        }
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 5);
 
         UrlData urlData = new UrlData(originalUrl, expirationTimeInSeconds);
         try {
+            String urlJson = objectMapper.writeValueAsString(urlData);
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket("my-bucket-1-zika")
+                    .key(shortUrlCode + ".json")
+                    .build();
 
-        } catch ()
-
-        Map<String, String> response = new HashMap<>();
-        response.put("code", shortUrlCode);
-
-        if (originalUrl == null || expirationTime == null) {
-            throw new RuntimeException("Unable to parse original URL");
+            s3Client.putObject(request, RequestBody.fromString(urlJson));
+        } catch (Exception e) {
+            throw new RuntimeException("Error saving URL data to S3" + e.getMessage(), e);
         }
 
+        Map<String, Object> response = new HashMap<>();
+        response.put("code", shortUrlCode);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+        headers.put("Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token");
+        headers.put("Access-Control-Allow-Methods", "OPTIONS,POST");
+        response.put("headers", headers);
+
         return response;
+    }
+
+    private Map<String, Object> buildErrorResponse(int statusCode, String message) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("statusCode", statusCode);
+        errorResponse.put("body", message);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+        errorResponse.put("headers", headers);
+
+        return errorResponse;
+
     }
 }
--- src/main/java/dev/otavio/UrlShorter/UrlData.java
+++ src/main/java/dev/otavio/UrlShorter/UrlData.java
@@ -3,7 +3,6 @@ package dev.otavio.UrlShorter;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
